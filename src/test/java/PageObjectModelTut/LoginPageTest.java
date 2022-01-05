f32cd0350fde604936fddd64dd96f9200f5747db
RajanRanjan <rajan.ranjan@oracle.com>
2022-01-05 14:07:16 +0530

    importing the missed package

--- src/test/java/PageObjectModelTut/LoginPageTest.java
+++ src/test/java/PageObjectModelTut/LoginPageTest.java
@@ -3,6 +3,7 @@ package PageObjectModelTut;
 import java.lang.reflect.Method;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
